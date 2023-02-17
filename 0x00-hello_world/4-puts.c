c5428ff108f4f5e72b58af6ffb70124eb7cc79b6
OneAmos1 <amosampah@gmail.com>
2023-02-17 14:25:42 -0800

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -print" Programming is like building a multilingual puzzle, followed by new line."
+ *
+ *return 0
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multiligual puzzle");
+  return(0);
+}
